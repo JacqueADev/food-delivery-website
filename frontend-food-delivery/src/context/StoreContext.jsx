74883ed69d770b8e7b76c2e03a705016acf3e2a2
JacqueADev <jacquelineadev@gmail.com>
2024-07-17 17:58:09 -0300

    css para foodItem e alterações no storecontext

--- frontend-food-delivery/src/context/StoreContext.jsx
+++ frontend-food-delivery/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext,  useState, useEffect } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext =
@@ -7,9 +7,35 @@ export const StoreContext =
 /*StoreContextProvider responsável por essa "caixa"*/
 const StoreContextProvider = (props) => {
   /*ContextValue o que será compartilhado - o que tem dentro da "caixa"*/
+
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev)=>({...prev, [itemId]:1}))
+        }
+        else {
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+  }
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+  }
+
+useEffect(()=>{
+    console.log(cartItems);
+},[cartItems]);
+
   const contextValue = {
-    food_list
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
   };
+
+
   return (
     /*StoreContext.Provider permite "compartilhar" esse conteúdo da caixa com outros*/
     <StoreContext.Provider value={contextValue}>
